cd3a9e461cc2a57497132d1394f724d97aa726e4
Mika Reich <mikareich0@gmail.com>
2020-09-18 22:59:50 +0200

    adding terser + lodash

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "./js/index.js",
   output: {
-    file: "./dist/index.bundle.js",
+    file: "./dist/index.min.js",
     format: "iife",
     name: "bundle",
   },
-  plugins: [babel({ exclude: "node_modules/*" })],
+  plugins: [
+    babel({ exclude: "node_modules/*" }),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
 };
