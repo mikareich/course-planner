dca6485cf51f7dc72e651b0b22a4230ff74aff6e
mikareich <mikareich0@gmail.com>
2020-09-20 21:05:48 +0200

    bundle css

--- js/index.js
+++ js/index.js
@@ -1,77 +1,77 @@
-import { categories, courseCombinations } from "./courses";
-
-const courseContainerDIV = document.querySelector(".course-container");
-let selectedCourses = [];
+import { categories, courseCombinations } from './courses'
+import '../css/style.css'
+const courseContainerDIV = document.querySelector('.course-container')
+let selectedCourses = []
 
 // create cours dom elements
-Object.values(categories).flat().forEach(createCourseElement);
+Object.values(categories).flat().forEach(createCourseElement)
 
 function createCourseElement(course) {
-  const element = document.createElement("div");
-  element.className = "course";
-  element.id = course;
-  element.innerHTML = `<span class="course-name">${course}</span>`;
-  courseContainerDIV.appendChild(element);
-  element.addEventListener("click", eventHandler);
+  const element = document.createElement('div')
+  element.className = 'course'
+  element.id = course
+  element.innerHTML = `<span class="course-name">${course}</span>`
+  courseContainerDIV.appendChild(element)
+  element.addEventListener('click', eventHandler)
 }
 
 function eventHandler(event) {
   const element =
-    event.target.id !== "" ? event.target : event.target.parentNode;
-  const course = element.id;
+    event.target.id !== '' ? event.target : event.target.parentNode
+  const course = element.id
 
   // cancel if disabled
-  if (element.classList.contains("disabled")) return;
+  if (element.classList.contains('disabled')) return
 
   // toggle selection
-  if (!element.classList.contains("selected")) {
-    element.classList.add("selected");
-    selectedCourses.push(course);
+  if (!element.classList.contains('selected')) {
+    element.classList.add('selected')
+    selectedCourses.push(course)
   } else {
-    element.classList.remove("selected");
-    selectedCourses.splice(selectedCourses.indexOf(course), 1);
+    element.classList.remove('selected')
+    selectedCourses.splice(selectedCourses.indexOf(course), 1)
   }
 
   // get further possibilities
-  const possibilities = findCoursePossibilties(selectedCourses);
+  const possibilities = findCoursePossibilties(selectedCourses)
   // disabled other items
-  courseContainerDIV.querySelectorAll(".course").forEach((el) => {
-    if (!possibilities.includes(el.id) && !el.classList.contains("selected"))
-      el.classList.add("disabled");
-    else el.classList.remove("disabled");
-  });
+  courseContainerDIV.querySelectorAll('.course').forEach((el) => {
+    if (!possibilities.includes(el.id) && !el.classList.contains('selected'))
+      el.classList.add('disabled')
+    else el.classList.remove('disabled')
+  })
 }
 
 function findCoursePossibilties(selectedCourses) {
   // convert given courses to categories
   const selectedCategories = selectedCourses.map((course) =>
     getCategory(course)
-  );
+  )
   // search for possibilities
   const possibleCategoryCombinatinons = courseCombinations.map(
     (courseCombination) => {
       // get categories of maped combi
       const combinationCategory = courseCombination.map((combi) =>
         getCategory(combi[0])
-      );
+      )
       // check if combi matches
       const matched = selectedCategories.every((category) => {
         if (combinationCategory.includes(category)) {
-          combinationCategory.splice(combinationCategory.indexOf(category), 1);
-          return true;
+          combinationCategory.splice(combinationCategory.indexOf(category), 1)
+          return true
         }
-      });
+      })
       if (matched)
         return combinationCategory
           .map((category) => categories[category])
-          .flat();
+          .flat()
     }
-  );
-  return [...new Set(possibleCategoryCombinatinons.flat().filter((n) => n))];
+  )
+  return [...new Set(possibleCategoryCombinatinons.flat().filter((n) => n))]
 }
 
 function getCategory(course) {
   return Object.entries(categories).filter(([category, courses]) =>
     courses.includes(course)
-  )[0][0];
+  )[0][0]
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,24 @@
-import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import scss from 'rollup-plugin-scss'
 
 export default {
-  input: "./js/index.js",
+  input: './js/index.js',
   watch: true,
   output: {
-    file: "./dist/index.min.js",
-    format: "iife",
-    name: "bundle",
+    file: './dist/bundle.js',
+    format: 'iife'
   },
   plugins: [
-    babel({ exclude: "node_modules/*" }),
+    babel({ exclude: 'node_modules/*' }),
     resolve(),
     commonjs(),
     terser(),
-  ],
-};
+    scss({
+      output: './dist/bundle.css',
+      outputStyle: 'compressed'
+    })
+  ]
+}
