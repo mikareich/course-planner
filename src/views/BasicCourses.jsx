6655a6cdcc06fbb4e42bc9ad126f66e0b670b715
mikareich <mikareich0@gmail.com>
2020-12-23 11:59:47 +0100

    display basic course

--- src/views/BasicCourses.jsx
+++ src/views/BasicCourses.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { SUBJECT_AREAS } from "../utils/courses";
+import Category from "../components/Category";
+import CourseItem from "../components/CourseItem";
 
 function BasicCourses() {
-  return <div className="basicCourses" />;
+  return (
+    <div className="basicCourses">
+      <div className="advancedCourses">
+        {Object.entries(SUBJECT_AREAS).map(([subject, coursesObject]) => {
+          const courses = (coursesObject.BasicCourses || coursesObject).flat();
+          return (
+            <Category label={subject} key={subject}>
+              {courses.flat().map((course) => (
+                <CourseItem name={course} type="basic-course" key={course} />
+              ))}
+            </Category>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default BasicCourses;
